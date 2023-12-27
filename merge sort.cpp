a4c35a47502f7c95e834e7bc71ecfb868f65b2a3
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2023-12-27 13:50:22 +0500

    Add files via upload

---
+++ merge sort.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+// Merge two subarrays of arr[].
+// First subarray is arr[left..mid]
+// Second subarray is arr[mid+1..right]
+void merge(int arr[], int left, int mid, int right) {
+    // Calculate sizes of two subarrays
+    int n1 = mid - left + 1;    // Size of left subarray
+    int n2 = right - mid;       // Size of right subarray
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the temporary arrays back into arr[]
+
+    // Initial indices of first and second subarrays
+    int i = 0, j = 0;
+
+    // Initial index of merged subarray
+    int k = left;
+
+    // Merge L[] and R[] back into arr[]
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Main function to sort an array arr[] using mergeSort
+void mergeSort(int arr[], int left, int right) {
+    if (left >= right)
+        return;
+
+    // Find the middle point
+    int mid = left + (right - left) / 2;
+
+    // Sort first and second halves
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    // Merge the sorted halves
+    merge(arr, left, mid, right);
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Given array is \n";
+    printArray(arr, arr_size);
+
+    // Call mergeSort function
+    mergeSort(arr, 0, arr_size - 1);
+
+    cout << "\nSorted array is \n";
+    printArray(arr, arr_size);
+    return 0;
+}
+
